fff0642143458dcb14668fd57667625417e6e98d
bjornwildenstam <bjorn.wildenstam@yh.nackademin.se>
2025-12-02 10:38:43 +0100

    defined tables and relationships in schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -38,3 +38,98 @@ export const identity = pgTable(
     ),
   })
 );
+
+export const consentCategory = pgTable(
+  "consent_category",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    website_id: uuid("website_id")
+      .notNull()
+      .references(() => websites.id),
+    key: text("key").notNull(),
+    description: text("description"),
+    is_required: boolean("is_required").notNull(),
+  },
+  (table) => ({
+    siteKeyUnique: unique("site_key_unique").on(table.website_id, table.key),
+  })
+);
+
+export const consentEvent = pgTable("consent_event", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  website_id: uuid("website_id")
+    .notNull()
+    .references(() => websites.id),
+  identity_id: uuid("identity_id")
+    .notNull()
+    .references(() => identity.id),
+  event_type: text("event_type").notNull(),
+  user_agent: text("user_agent"),
+  policy_version_text: text("policy_version_text"),
+  created_at: timestamp("created_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+});
+
+export const consentChoice = pgTable("consent_choice", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  consent_event_id: uuid("consent_event_id")
+    .notNull()
+    .references(() => consentEvent.id, { onDelete: "cascade" }),
+  consent_category_id: uuid("consent_category_id")
+    .notNull()
+    .references(() => consentCategory.id),
+  status: boolean("status").notNull(),
+});
+
+// Relationer
+export const websiteRelations = relations(websites, ({ many }) => ({
+  // En hemsida har många identiteter, kategorier och event
+  identities: many(identity),
+  categories: many(consentCategory),
+  events: many(consentEvent),
+}));
+
+export const identityRelations = relations(identity, ({ one, many }) => ({
+  // En identitet/användare tillhör En hemsida
+  website: one(websites, {
+    fields: [identity.website_id],
+    references: [websites.id],
+  }),
+  // En identitet har MÅNGA samtyckeshändelser
+  consentEvents: many(consentEvent),
+}));
+
+export const consentEventRelations = relations(
+  consentEvent,
+  ({ one, many }) => ({
+    // Ett event tillhör EN identitet
+    identity: one(identity, {
+      fields: [consentEvent.identity_id],
+      references: [identity.id],
+    }),
+    // Ett event har MÅNGA val (choice)
+    choices: many(consentChoice),
+  })
+);
+
+export const consentChoiceRelations = relations(consentChoice, ({ one }) => ({
+  //Ett val tillhör ETT event
+  event: one(consentEvent, {
+    fields: [consentChoice.consent_event_id],
+    references: [consentEvent.id],
+  }),
+  // Ett val tillhör EN kategori
+  category: one(consentCategory, {
+    fields: [consentChoice.consent_category_id],
+    references: [consentCategory.id],
+  }),
+}));
+
+export const consentCategoryRelations = relations(
+  consentCategory,
+  ({ many }) => ({
+    // En kategori har MÅNGA val
+    choices: many(consentChoice),
+  })
+);
